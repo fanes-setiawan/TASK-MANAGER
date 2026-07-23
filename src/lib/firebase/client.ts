0ecceb2a05a24dc871b7f45535d75630f4b9ccb4
fanes-setiawan <fanessetiawan.1401@gmail.com>
2026-07-23 23:02:43 +0700

    Fallback to public Firebase variables to fix Cloudflare build error

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCjY1BtMXbFMzgJHxcqFqijJwGpC32tkJA",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "task-manager-b52bb.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "task-manager-b52bb",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "task-manager-b52bb.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "326695732751",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:326695732751:web:573dcabc3b98eef9f9857a",
+  measurementId: "G-FHXQFPVRNB"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
