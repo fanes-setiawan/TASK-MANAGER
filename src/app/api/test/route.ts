57e258af4490f3117e045f50998653ae47dae142
fanes-setiawan <fanessetiawan.1401@gmail.com>
2026-08-06 16:32:59 +0700

    chore: test firebase admin import in vercel

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server';
+import { getFirestore } from 'firebase-admin/firestore';
 
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-  return NextResponse.json({ message: 'API is working' }, { status: 200 });
+  return NextResponse.json({ message: 'API is working', fb: typeof getFirestore }, { status: 200 });
 }
